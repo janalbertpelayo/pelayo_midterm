5443dd8e9b1c0cc14ec017647da28a2aaf6e1294
3R3_Pelayo <janalbertpelayo@gmail.com>
2025-04-03 17:17:25 +0800

    2 files created and final working api.php
    Http/Kernel.php
    Http/Middleware/VerifyCsrfToken.php

---
+++ app/Http/Kernel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \App\Http\Middleware\VerifyCsrfToken::class, // CSRF Middleware
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('product/create', [ProductController::class, 'create'])->name('product.create');
+Route::get('product/create', [ProductController::class, 'create']);
 Route::post('product', [ProductController::class, 'store']);
